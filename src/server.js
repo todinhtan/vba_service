d7b434f9e60bb1d9f51f3010e51d073b77886fcc
Tan <tan@intersquad.co>
2019-01-21 23:08:33 +0700

    add basic authen

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import compression from 'compression';
+import basicAuth from 'express-basic-auth';
 
 import config from './config';
 import logger from './utils/logger';
@@ -16,6 +17,9 @@ api.use(compression());
 api.use(bodyParser.urlencoded({ extended: true }));
 api.use(bodyParser.json());
 api.use('/v2/health', healthcheck());
+api.use(basicAuth({
+  users: { admin: 'adm1nadm1n' },
+}));
 require('./routes/vba')(api);
 
 api.listen(config.server.port, (err) => {
