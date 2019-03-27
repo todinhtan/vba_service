f168867a1e053ebc4728e1708a69f08356021573
Tan <tan@intersquad.co>
2019-03-27 09:44:02 +0700

    add access log

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import compression from 'compression';
 import basicAuth from 'express-basic-auth';
+import morgan from 'morgan';
+import fs from 'fs';
+import path from 'path';
 
 import config from './config';
 import logger from './utils/logger';
@@ -10,13 +13,17 @@ import logger from './utils/logger';
 require('./utils/db');
 const healthcheck = require('express-healthcheck');
 
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+morgan.token('reqBody', (req, res) => JSON.stringify(req.body));
+
 const api = express();
 
-api.use('*', cors());
-api.use(compression());
-api.use(bodyParser.urlencoded({ extended: true }));
-api.use(bodyParser.json());
-api.use('/v2/health', healthcheck());
+api.use('*', cors())
+  .use(compression())
+  .use(morgan(':method :url :status :reqBody - :response-time ms', { stream: accessLogStream }))
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(bodyParser.json())
+  .use('/v2/health', healthcheck());
 // api.use(basicAuth({
 //   users: { admin: '123456@stg' },
 // }));
